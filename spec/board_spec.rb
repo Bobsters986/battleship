5487e0db51fa1e1cf6b19cbe3d1f3dae3f54933a
Bobby Luly <bobbyluly@gmail.com>
2022-12-07 21:38:31 -0700

    Test- pass valid placement p1

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -7,6 +7,7 @@ RSpec.describe Board do
         board = Board.new
         board.cells
 
+        expect(board).to be_an_instance_of(Board)
         expect(board.cells).to be_a(Hash)
         expect(board.cells.count).to eq(16)
         # expect(board.cells.keys[0]).to eq(cells.coordinate)
@@ -21,6 +22,33 @@ RSpec.describe Board do
         expect(board.valid_coordinate?("A5")).to eq(false)
         expect(board.valid_coordinate?("E1")).to eq(false)
         expect(board.valid_coordinate?("A22")).to eq(false)
+    end
+
+    it "has valid placements for ships" do
+        board = Board.new
+        cruiser = Ship.new("Cruiser", 3)
+        submarine = Ship.new("Submarine", 2)
+
+        board.cells
+        
+        # number of coordinates in array should be same as the length of the ship
+        expect(board.valid_placement?(cruiser, ["A1", "A2"])).to be false
+        expect(board.valid_placement?(submarine, ["A2", "A3", "A4"])).to be false
+
+        # the coordinates are consecutive
+        # expect(board.valid_placement?(cruiser, ["A1", "A2", "A4"])).to be false
+        # expect(board.valid_placement?(submarine, ["A1", "C1"])).to be false
+        # expect(board.valid_placement?(cruiser, ["A3", "A2", "A1"])).to be false
+        # expect(board.valid_placement?(submarine, ["C1", "B1"])).to be false
+
+        # coordinates can’t be diagonal
+        # expect(board.valid_placement?(cruiser, ["A1", "B2", "C3"])).to be false
+        # expect(board.valid_placement?(submarine, ["C2", "D3"])).to be false
+
+        # If all the previous checks pass then the placement should be valid:
+        # expect(board.valid_placement?(submarine, ["A1", "A2"])).to be true
+        # expect(board.valid_placement?(cruiser, ["B1", "C1", "D1"])).to be true
+
     end
 
 end
