9442862327b5170ea69cb643a432342d5dfc57a8
Bobby Luly <bobbyluly@gmail.com>
2022-12-07 19:14:24 -0700

    Test valid coordinates and pass

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,6 +1,6 @@
-require './lib/ship'
-require './lib/cell'
 require './lib/board'
+require './lib/cell'
+require './lib/ship'
 
 RSpec.describe Board do
     it "exists" do
@@ -10,6 +10,17 @@ RSpec.describe Board do
         expect(board.cells).to be_a(Hash)
         expect(board.cells.count).to eq(16)
         # expect(board.cells.keys[0]).to eq(cells.coordinate)
-        
     end
+
+    it "has valid coordinates" do
+        board = Board.new
+        board.cells
+
+        expect(board.valid_coordinate?("A1")).to eq(true)
+        expect(board.valid_coordinate?("D4")).to eq(true)
+        expect(board.valid_coordinate?("A5")).to eq(false)
+        expect(board.valid_coordinate?("E1")).to eq(false)
+        expect(board.valid_coordinate?("A22")).to eq(false)
+    end
+
 end
