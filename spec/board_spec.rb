2da07b5d607d6ffea2bb8fe13f12b16dc028293f
Bobby Luly <bobbyluly@gmail.com>
2022-12-08 21:53:42 -0700

    Test- pass diagonal and valid placement, Add 2 more tests to pass

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -10,7 +10,6 @@ RSpec.describe Board do
         expect(board).to be_an_instance_of(Board)
         expect(board.cells).to be_a(Hash)
         expect(board.cells.count).to eq(16)
-        # expect(board.cells.keys[0]).to eq(cells.coordinate)
     end
 
     it "has valid coordinates" do
@@ -31,13 +30,11 @@ RSpec.describe Board do
 
         board.cells
         
-        # number of coordinates in array should be same as the length of the ship
         expect(board.valid_placement?(cruiser, ["A1", "A2"])).to be false
         expect(board.valid_placement?(submarine, ["A2", "A3", "A4"])).to be false
     end
 
     it "coordinates are consecutive" do
-        # the coordinates are consecutive
         board = Board.new
         cruiser = Ship.new("Cruiser", 3)
         submarine = Ship.new("Submarine", 2)
@@ -49,7 +46,6 @@ RSpec.describe Board do
     end
 
     it "coordinates can't be diagonal" do
-        # # coordinates can’t be diagonal
         board = Board.new
         cruiser = Ship.new("Cruiser", 3)
         submarine = Ship.new("Submarine", 2)
@@ -59,21 +55,34 @@ RSpec.describe Board do
     end
 
     it "all checks pass and have valid placements" do
-        # # If all the previous checks pass then the placement should be valid:
         board = Board.new
         cruiser = Ship.new("Cruiser", 3)
         submarine = Ship.new("Submarine", 2)
 
-         expect(board.valid_placement?(submarine, ["A1", "A2"])).to be true
-         expect(board.valid_placement?(cruiser, ["B1", "C1", "D1"])).to be true
+        expect(board.valid_placement?(submarine, ["A1", "A2"])).to be true
+        expect(board.valid_placement?(cruiser, ["B1", "C1", "D1"])).to be true
     end
 
-    xit "checks for overlapping ships and doesn't allow" do
+    xit "placing ships" do
         board = Board.new
         cruiser = Ship.new("Cruiser", 3)
 
         board.place(cruiser, ["A1", "A2", "A3"])
+        cell_1 = board.cells["A1"]  
+        cell_2 = board.cells["A2"]
+        cell_3 = board.cells["A3"]
 
+        expect(cell_1.ship).to eq(cruiser)
+        expect(cell_2.ship).to eq(cruiser)
+        expect(cell_3.ship).to eq(cruiser)
+        expect(cell_3.ship == cell_2.ship).to be true
+    end
+
+    xit "prevent overlapping ships" do
+        board = Board.new
+        cruiser = Ship.new("Cruiser", 3)
+
+        board.place(cruiser, ["A1", "A2", "A3"])
         submarine = Ship.new("Submarine", 2)
 
         expect(board.valid_placement?(submarine, ["A1", "B1"])).to be false
