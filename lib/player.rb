dfc898d18a557095ed496ca7b1d4dca04d4388f5
Bobby Luly <bobbyluly@gmail.com>
2022-12-12 10:43:32 -0700

    Feat- initialize Player class and create has_lost? method

--- lib/player.rb
+++ lib/player.rb
@@ -1,6 +1,19 @@
-class Player
+require './lib/board'
+require './lib/cell'
+require './lib/ship'
 
-    def initialize(name, board, )
+class Player
+    attr_reader :name, :grid, :big_ship, :small_ship
+
+    def initialize(name, grid, big_ship, small_ship)
+        @name = name
+        @grid = grid
+        @big_ship = big_ship
+        @small_ship = small_ship
+    end
+
+    def has_lost?
+        @big_ship.sunk? && @small_ship.sunk?
     end
 
 end
