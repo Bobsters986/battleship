b22da026f15a9a3af231178945dcc2a12cf9fc39
Bobby Luly <bobbyluly@gmail.com>
2022-12-12 20:03:30 -0700

    Fix- change player to user

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -1,45 +1,46 @@
-require './lib/board'
-require './lib/cell'
 require './lib/ship'
+require './lib/cell'
+require './lib/board'
 require './lib/player'
 
 RSpec.describe Player do
-    it "can add new players" do
-        player_board = Board.new
+    it "can add new user" do
+        user_board = Board.new
         computer_board = Board.new
         cruiser_1 = Ship.new("Cruiser", 3)
         sub_1 = Ship.new("Submarine", 2)
         cruiser_2 = Ship.new("Cruiser", 3)
         sub_2 = Ship.new("Submarine", 2)
-        player = Player.new('Bobby', player_board, cruiser_1, sub_1)
+        user = Player.new('Bobby', user_board, cruiser_1, sub_1)
         computer = Player.new('Computer', computer_board, cruiser_2, sub_2)
 
-        expect(player.name).to eq('Bobby')
-        expect(player.grid).to eq(player_board)
-        expect(player.big_ship).to eq(cruiser_1)
-        expect(player.small_ship).to eq(sub_1)
+        expect(user.name).to eq('Bobby')
+        expect(user.grid).to eq(user_board)
+        expect(user.big_ship).to eq(cruiser_1)
+        expect(user.small_ship).to eq(sub_1)
     end
 
-    it "can tell when a player/computer has lost" do
-        player_board = Board.new
+    it "can tell when a user/computer has lost" do
+        user_board = Board.new
         computer_board = Board.new
         cruiser_1 = Ship.new("Cruiser", 3)
         sub_1 = Ship.new("Submarine", 2)
         cruiser_2 = Ship.new("Cruiser", 3)
         sub_2 = Ship.new("Submarine", 2)
-        player = Player.new('Bobby', player_board, cruiser_1, sub_1)
+        user = Player.new('Bobby', user_board, cruiser_1, sub_1)
         computer = Player.new('Computer', computer_board, cruiser_2, sub_2)
 
-        expect(player.has_lost?).to be false
+        expect(user.has_lost?).to be false
 
         cruiser_1.hit
         cruiser_1.hit
         cruiser_1.hit
-        expect(player.has_lost?).to be false
+        expect(cruiser_1.sunk?).to be true
 
         sub_1.hit
         sub_1.hit
-        expect(player.has_lost?).to be true
+        expect(sub_1.sunk?).to be true
+        expect(user.has_lost?).to be true
     end
 
 end
